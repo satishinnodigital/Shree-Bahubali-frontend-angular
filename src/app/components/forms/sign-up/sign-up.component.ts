ac88523baef6b8305e10b6f965c3ac0695df1e4a
ramuinnodigital <ramu@innodigital.in>
2023-12-20 17:20:14 +0530

    signup page

--- src/app/components/forms/sign-up/sign-up.component.ts
+++ src/app/components/forms/sign-up/sign-up.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AbstractControl, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-sign-up',
@@ -6,5 +7,34 @@ import { Component } from '@angular/core';
   styleUrls: ['./sign-up.component.scss']
 })
 export class SignUpComponent {
+
+  form: FormGroup; 
+  submitted = false;
+
+  constructor(private formBuilder: FormBuilder) {
+    this.form = this.formBuilder.group({
+      mobile: new FormControl('',[Validators.required,Validators.pattern("^((\\+91-?)|0)?[0-9]{10}$")]),
+      email: new FormControl('',[Validators.required, Validators.email]),
+      acceptTerms: new FormControl('',Validators.required),
+    });
+  }
+
+  ngOnInit(): void {
+  
+  }
+
+  get f(): { [key: string]: AbstractControl } {
+    return this.form.controls;
+  }
+
+  onSubmit(): void {
+    this.submitted = true;
+
+    if (this.form.valid) {
+     
+    }
+
+    
+  }
 
 }
