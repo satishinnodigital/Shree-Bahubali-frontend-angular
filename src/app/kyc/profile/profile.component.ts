104d149d35b80dc21dfad4341756df948c2bfe6f
satishinnodigital <satish@innodigital.in>
2023-12-28 16:11:36 +0530

    profile UI added

--- src/app/kyc/profile/profile.component.ts
+++ src/app/kyc/profile/profile.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-profile',
@@ -7,4 +8,29 @@ import { Component } from '@angular/core';
 })
 export class ProfileComponent {
 
+
+  addNominee = false;
+  profileForm!:FormGroup;
+
+
+  constructor(private formBuilder:FormBuilder){
+    this.profileForm =this.formBuilder.group({
+      panUpload:new FormControl(""),
+      signatureUpload:new FormControl(""),
+      maritalStatus:new FormControl(""),
+      exp:new FormControl(""),
+      occupation:new FormControl(""),
+      annualIncome:new FormControl(""),
+      fatherName:new FormControl(""),
+      spouseName:new FormControl(""),
+      motherFirstName:new FormControl(""),
+      motherMiddleName:new FormControl(""),
+      motherLastName:new FormControl(""),
+      nominee:new FormControl(""),
+      politicallyExpose:new FormControl(""),
+      qualification:new FormControl(""),
+      investWith:new FormControl(""),
+      accSettelement: new FormControl("")
+    })
+  }
 }
