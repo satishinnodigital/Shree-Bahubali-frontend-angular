89313b2c5e865d0c838ef7954f89cf35452782d1
satishinnodigital <satish@innodigital.in>
2023-12-28 18:04:38 +0530

    exchange UI added

--- src/app/kyc/profile/profile.component.ts
+++ src/app/kyc/profile/profile.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
+import { AbstractControl, FormArray, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-profile',
@@ -11,6 +11,7 @@ export class ProfileComponent {
 
   addNominee = false;
   profileForm!:FormGroup;
+  empForm!:FormGroup;
 
 
   constructor(private formBuilder:FormBuilder){
@@ -26,11 +27,92 @@ export class ProfileComponent {
       motherFirstName:new FormControl(""),
       motherMiddleName:new FormControl(""),
       motherLastName:new FormControl(""),
-      nominee:new FormControl(""),
+      nomineeDetails: this.formBuilder.array([]),
       politicallyExpose:new FormControl(""),
       qualification:new FormControl(""),
       investWith:new FormControl(""),
       accSettelement: new FormControl("")
     })
+
+    this.empForm =  this.formBuilder.group({
+      name:new FormControl("",[Validators.required]),
+      skills:this.formBuilder.array([]),
+      educationDetails:this.formBuilder.array([])
+    })
+    this.skills.push(this.newSkill());
+
+  }
+
+  get nomineeDetails():FormArray{
+    return this.profileForm.get("nomineeDetails") as FormArray
+  }
+
+
+  addNewNominee():FormGroup{
+    return this.formBuilder.group({
+
+    })
+  }
+
+  get f(): { [key: string]: AbstractControl } {
+    return this.empForm.controls;
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  get skills() : FormArray {
+    return this.empForm.get("skills") as FormArray
+  }
+
+  newSkill():FormGroup{
+    return this.formBuilder.group({
+      skill:new FormControl("",[Validators.required]),
+      exp:new FormControl("",[Validators.required]),
+    })
+  }
+
+  addSkills(){
+    this.skills.push(this.newSkill());
+  }
+
+  removeSkill(i:number){
+    this.skills.removeAt(i);
+  }
+  submitted = false
+  onSubmit() {
+    this.submitted = true
+    if(this.empForm.valid){
+      console.log(this.empForm.value);
+      alert("Valid")
+    }
+    console.log(this.empForm.value);
   }
 }
